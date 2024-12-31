ac67735ec1e6be91ac757bc803961612c2d36a18
Paul Robson <paul@robsons.org.uk>
2024-12-31 09:07:23 +0000

    FZX font conversion added

--- images/scripts/fonts/convert.py
+++ images/scripts/fonts/convert.py
@@ -10,9 +10,37 @@
 # ***************************************************************************************
 # ***************************************************************************************
 
+import re,os,sys
+
 # ***************************************************************************************
 #
 #							Represent a single character bitmap
+#
+# ***************************************************************************************
+#
+#		Bitmap character Data
+#		=====================
+#
+#		Rendering Width (in pixels, 8 means double bytes of data)
+#		Kerning (MSN) Extra Spacing (LSN)
+#		Baseline offset from top.
+#		# Blank Lines above character
+# 		# Lines of character data
+#		# Blank Lines below data
+#		Binary Data (multiple)
+#
+# ***************************************************************************************
+#
+#		Font Data
+#		=========
+#
+#		Height (the recorded height - 1)
+#		First character
+#		Last character
+#		(Pad to 16 bytes)
+#		Offset from start of font data to first character (or $FFFF if character not defined)
+#
+#		Header size is 16 + (number of characters) * 2
 #
 # ***************************************************************************************
 
@@ -33,10 +61,46 @@ class FontCharacter(object):
 			if self.bytes == 2:
 				self.data[-1] = self.data[-1] | data.pop(0)
 
+	def getExtent(self):
+		self.startData = 0
+		self.endData = len(self.data)-1
+		while self.startData < len(self.data) and self.data[self.startData] == 0:
+			self.startData += 1
+		while self.endData >= 0 and self.data[self.endData] == 0:
+			self.endData -= 1
+		self.startData = min(self.startData,len(self.data))
+		self.endData = max(self.startData,self.endData)
+
+	def renderData(self,baseLineOffset,extraSpace):
+		self.renderData =  [ self.width,self.kerning * 16 + extraSpace,baseLineOffset,self.startData,self.endData-self.startData+1,len(self.data)-self.endData-1]
+
+		self.renderData.append(0)   												# Set up first byte, unused
+		bitMask = 0x80
+
+		#print(self.startData,self.endData,len(self.data))
+		for y in range(self.startData,self.endData+1):   							# Scan vertically, then horizontally
+			for x in range(0,self.width): 
+				if y < len(self.data):
+					if (self.data[y] & (0x8000 >> x)) != 0:   						# Pixel set, set that bit
+						self.renderData[-1] |= bitMask
+
+				bitMask >>= 1   														# Shift to next bit
+				if bitMask == 0:
+					bitMask = 0x80
+					self.renderData.append(0)
+
+		#print(self.renderData)
+		#self.print()
+
+	def get(self):
+		return self.renderData
+
 	def print(self):
+		n = 0
 		for d in self.data:
 			s = "".join(["*" if (d & (0x8000 >> i) != 0) else "." for i in range(0,16)])
-			print(s)
+			print(n,s)
+			n += 1
 		print()
 
 # ***************************************************************************************
@@ -48,12 +112,14 @@ class FontCharacter(object):
 class Font(object):
 	def __init__(self,source):
 		self.data = [x for x in open(source,"rb").read(-1)]  						# Read in the font
+		self.name = source.split(os.sep)[-1].lower().replace(".fzx","")  			# Store processed name
 		self.height = self.data[0]  												# CR height
 		self.xSpacing = self.data[1]   												# Additional width.
+		self.firstChar = 32
 		self.lastChar = self.data[2] 												# End character
 		self.chars = {}
 
-		for i in range(32,self.lastChar+1):
+		for i in range(self.firstChar,self.lastChar+1):
 			charEntry = 3 + (i-32) * 3  											# Offset todata
 
 			kerning = self.data[charEntry+1] >> 6  									# Kerning 
@@ -67,12 +133,67 @@ class Font(object):
 			dataNext = self.getAddr(charEntry+3)
 			newChar.addRows(self.data[data:dataNext])  								# Add that data.
 			newChar.pad(self.height)  												# Pad out new character with filler
+			newChar.getExtent()
 
-			newChar.print()
+	def render(self):
+		self.baseLine = self.chars[ord('o')].endData  								# This is the base line for the font.
+
+		for c in self.chars.keys():   												# Convert data to pixels
+			self.chars[c].renderData(self.baseLine,self.xSpacing)
+			#self.chars[c].print()
+
+		count = self.lastChar - self.firstChar + 1   								# No of records.
+		self.data = [ self.height-1, self.firstChar,self.lastChar,0,0,0,0,0] 		# Header
+		self.data += [ 0xFF ] * (count * 2)   										# Blank records 
+
+		for c in range(self.firstChar,self.lastChar+1):  							# For each character
+			aPos = 8 + (c - self.firstChar) * 2   									# Update the offset table.
+			self.data[aPos] = len(self.data) & 0xFF
+			self.data[aPos+1] = len(self.data) >> 8
+			self.data += self.chars[c].get()    									# Append created data.
+		return self
 
 	def getAddr(self,pos):
 		return pos + (self.data[pos])+(self.data[pos+1] & 0x3F) * 256
 
+	def getFontName(self):
+		return self.name 
 
-f = Font("fonts/Hemery.fzx")		
+	def getFontIdentifier(self):
+		return "_"+self.getFontName()+"_data";
+
+# ***************************************************************************************
+#
+#							Represent a font collection
+#
+# ***************************************************************************************
+
+class FontCollection(object):
+	def __init__(self,directory):
+		self.fonts = {}
+		for root,dirs,files in os.walk(directory):
+			for f in files:
+				if f.endswith(".fzx"):
+					nc = Font(root+os.sep+f)
+					self.fonts[nc.getFontName()] = nc
+
+	def render(self):
+		print("#pragma once\n\n")
+		keys = [x for x in self.fonts.keys()]
+		keys.sort()
+		size = 0
+		for f in keys:
+			font = self.fonts[f]
+			print("// {0} bytes\n".format(len(font.data)))
+			print("static const uint8_t {0}[] = {{ {1} }};\n\n".format(font.getFontIdentifier(),",".join([str(x) for x in font.data])))
+			size += len(font.data)
+		print("static int fontCount = {0};\n".format(len(keys)))
+
+		print("static const uint8_t *fontDataIndex[] = {{ {0} }};\n\n".format(",".join([self.fonts[x].getFontIdentifier() for x in keys])))
+
+		print("static const char *fontNameIndex[] = {{ {0} }};\n\n".format(",".join(['"'+self.fonts[x].getFontName()+'"' for x in keys])))
+
+		print("// Size {0}".format(size))
+		
+FontCollection("fonts").render()
 
--- libraries/graphics/fonts.c
+++ libraries/graphics/fonts.c
@@ -3,6 +3,7 @@
 //
 //      Name :      fonts.c
 //      Authors :   Paul Robson (paul@robsons.org.uk)
+//                  Andrew S. Owen (Font Design)
 //      Date :      30th December 2024
 //      Reviewed :  No
 //      Purpose :   Monochrome Font Rendering
@@ -15,23 +16,11 @@
 
 // ***************************************************************************************
 //
-//		Include the font data in Flash memory. This uses the ADAFruit format (see 
-//		Adafuit_GFX.cpp lines 1134-1215 (at present)) method Adafruit_GFX::drawChar
+//		                    Include the font data in Flash memory. 
 //
 // ***************************************************************************************
 
-#include "fonts/fonts.h"
-
-// ***************************************************************************************
-//
-//          Get the Glyph for the given character and font, NULL if not found.
-//
-// ***************************************************************************************
-
-static GFXglyph *GFXGetGlyph(const GFXfont *font,int c) {
-    if (c < font->first || c > font->last) return NULL;
-    return &(font->glyph[c-font->first]);
-}
+#include "__fonts.h"
 
 // ***************************************************************************************
 //
@@ -42,34 +31,7 @@ static GFXglyph *GFXGetGlyph(const GFXfont *font,int c) {
 static int _DrawCharacter(GFXPort *vp,int xPos,int yPos,int ch,int colour,int scale) {
 
     GFXASetPort(vp);
-    const GFXfont *font = &freesans7pt7b;
-
-    GFXglyph *glyph = GFXGetGlyph(font,ch);                                         // Extract the glyph, exit if not available
-    if (glyph == NULL) return 0;
-    
-    xPos = xPos+glyph->xOffset*scale;                                               // Adjust the drawing point by the offset
-    yPos = yPos+glyph->yOffset*scale;
-
-    uint8_t bitCount = 0;                                                           // Track when we need another byte
-    int byteOffset = glyph->bitmapOffset;                                           // Offset into byte data
-    uint8_t currentByte = 0xFF;                                                     // Current byte being used
-
-    for (int y = 0;y < glyph->height;y++) {                                         // Do vertical first
-        for (int x = 0;x < glyph->width;x++) {                                      // Then horizontal
-            if ((bitCount & 7) == 0) currentByte = font->bitmap[byteOffset++];      // Do we need another byte
-            bitCount++;
-
-            if ((currentByte & 0x80) != 0) {                                        // If bit on.
-                if (scale == 1) {
-                    GFXAPlot(xPos+x*scale,yPos+y*scale,colour);
-                } else {
-                    GFXFillRect(vp,xPos+x*scale,yPos+y*scale,xPos+x*scale+scale-1,yPos+y*scale+scale-1,colour);
-                }
-            }
-            currentByte <<= 1;                                                      // Access next byte
-        }
-    }
-    return glyph->xAdvance;
+    return 0;
 }
 
 void GFXDrawString(GFXPort *vp,int xPos,int yPos,char *s,int colour,int scale) {
